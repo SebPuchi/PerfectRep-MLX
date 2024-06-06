2cdce2420300cec00e585ba4274c9f93f61f794f
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-06-06 17:53:08 -0600

    basic V1 Data model implemented

--- PerfectRep/Data/Data Models/Versions/PerfectRepV1.swift
+++ PerfectRep/Data/Data Models/Versions/PerfectRepV1.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import CoreStore
+
+
+enum PerfectRepV1 {
+    
+    static let identifier = "PerfectRepV1"
+    static let schema = CoreStoreSchema(
+        modelVersion: PerfectRepV1.identifier,
+        entities: [
+            Entity<PerfectRepV1.Workout>(PerfectRepV1.Workout.identifier)
+        ]
+    )
+    
+    // MARK: Workout CoreStore Obj
+    class Workout: CoreStoreObject {
+        
+        static let identifier = "Workout"
+        
+        let uuid = Value.Optional<UUID>("id")
+        let workoutType = Value.Required<Int>("workoutType", initial: -1)
+        let startDate = Value.Optional<Date>("startDate")
+        let dayIdentifier = Value.Optional<String>("dayIdentifier")
+        let burnedEnergy = Value.Optional<Double>("burnedEnergy")
+        let healthKitUUID = Value.Optional<UUID>("healthKitID")
+        
+    }
+    
+    
+    
+}
