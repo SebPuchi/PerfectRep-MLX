c2e31e69bb43aeedced8ad783f77e648eae047cf
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-06-06 11:45:47 -0600

    Initial setup swift

---
+++ PerfectRep/PerfectRepUITests/PerfectRepUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PerfectRepUITestsLaunchTests.swift
+//  PerfectRepUITests
+//
+//  Created by Sebastian Pucher on 6/6/24.
+//
+
+import XCTest
+
+final class PerfectRepUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
