ac2229bf39b531be508c7f30cffd87ca1480a626
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-06-10 12:12:14 -0600

    settings model set up

---
+++ PerfectRep/Models/Settings/SettingsModel.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsModel.swift
+//  PerfectRep
+//
+//  Created by Sebastian Pucher on 6/10/24.
+//
+
+import Foundation
+import UIKit
+import MobileCoreServices
+
+class SettingsModel {
+    
+  
+    
+}
