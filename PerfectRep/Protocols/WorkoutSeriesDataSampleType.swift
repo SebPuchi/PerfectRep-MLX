f53b17f44689c4082eef06e4da6c20a7b60a6c90
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-06-20 10:43:17 -0600

    WorkoutSeriesData / BigStatView

---
+++ PerfectRep/Protocols/WorkoutSeriesDataSampleType.swift
@@ -0,0 +1,10 @@
+//
+//  WorkoutSeriesDataSampleType.swift
+//  PerfectRep
+//
+//  Created by Sebastian Pucher on 6/20/24.
+//
+
+import Foundation
+
+protocol WorkoutSeriesDataSampleType: CoreStoreObject {}
