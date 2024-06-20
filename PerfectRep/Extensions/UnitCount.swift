a961af8f86fcb021e3a4986b9652ec7e02cd4a00
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-06-20 10:20:39 -0600

    UnitCount

---
+++ PerfectRep/Extensions/UnitCount.swift
@@ -0,0 +1,14 @@
+//
+//  UnitCount.swift
+//  PerfectRep
+//
+//  Created by Sebastian Pucher on 6/20/24.
+//
+
+import Foundation
+
+class UnitCount {
+    
+    static let count = Unit(symbol: "")
+    
+}
